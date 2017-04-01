3fa870eefc123fe9dd6370da20dae245cd730e3f
Lucsaan <andreas.schmieder@googlemail.com>
2017-04-01 17:35:14 +0200

    Embedded Youtube Videos

--- src/app/video-page/video-page.component.ts
+++ src/app/video-page/video-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { DomSanitizer} from '@angular/platform-browser';
 
 @Component({
   selector: 'app-video-page',
@@ -6,10 +7,38 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./video-page.component.css']
 })
 export class VideoPageComponent implements OnInit {
-
-  constructor() { }
+  videoArray: any = [
+    {
+      name: 'JDK & Eclipse',
+      id: 'b1pDj1gx9zs'
+    },
+    {
+      name: 'System.out.Print',
+      id: 'ghDEieyOr8o'
+    },
+    {
+      name: 'Einführung von Int',
+      id: '0N5Xc3oQ2-U'
+    }
+  ];
+  url: any;
+  name:string;
+  video: any = {id: '0N5Xc3oQ2-U'};
+  baseUrl:string = 'https://www.youtube.com/embed/';
+  constructor(private sanitizer: DomSanitizer) { 
+    this.url = this.sanitizer.bypassSecurityTrustResourceUrl(this.baseUrl + this.video.id);    
+  }
 
   ngOnInit() {
   }
+
+  showVideo(id){
+    this.video.id = id;
+    this.url = this.sanitizer.bypassSecurityTrustResourceUrl(this.baseUrl + this.video.id);    
+  
+  }
+
+  
+
 
 }
