34920cebab6fcb2257e6ec0a276700ce581bb3f4
Lucsaan <andreas.schmieder@googlemail.com>
2017-04-01 13:50:05 +0200

    Routing Setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,30 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { MainPageComponent } from './main-page/main-page.component';
+
+const appRoutes : Routes = [
+  { path: 'MainPage', component: MainPageComponent },
+  { path: '', 
+    redirectTo: '/TutorPage',
+    pathMatch: 'full'
+  },
+  { path: '**', component: MainPageComponent}
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainPageComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
