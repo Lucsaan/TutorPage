bd9b6f3d6adb339b2a8343934f2b17cac53c9022
chGoodchild <ch.goodchild@gmail.com>
2017-04-06 03:00:52 +0200

    Added content

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Chandran isch de bescht Tutor dens gibt :-)';
+  title = 'INFO 1 TUTORATE';
 }
